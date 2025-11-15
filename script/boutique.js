564a8d40aa581d4045d006037416a4905ee0aee7
alexis-pag <alexisp57140@gmail.com>
2025-11-15 11:29:48 +0100

    Update boutique.js

--- script/boutique.js
+++ script/boutique.js
@@ -4,17 +4,17 @@
 (() => {
   window.storeItemsData = [
     { name: "Gamelle de croquette", price: 10, mult: 1, auto: 0, owned: 0, icon: "script/image/gamelle.png" },
-    { name: "Cage à croquette", price: 25, mult: 5, auto: 0, owned: 0, icon: "script/image/cage.png" },
-    { name: "Paquet de croquette", price: 50, mult: 0, auto: 1, owned: 0, icon: "script/image/paquet.png" },
-    { name: "Magasin de croquettes", price: 200, mult: 0, auto: 5, owned: 0, icon: "script/image/magasin.png" },
-    { name: "Supermarcher de croquette", price: 500, mult: 0, auto: 10, owned: 0, icon: "script/image/supermarche.png" },
-    { name: "Hypermarcher de croquettes", price: 1000, mult: 0, auto: 20, owned: 0, icon: "script/image/hypermarcher.png" },
-    { name: "Entrepot de croquettes", price: 5000, mult: 0, auto: 50, owned: 0, icon: "script/image/entrepot.png" },
-    { name: "Usine de croquettes", price: 10000, mult: 0, auto: 100, owned: 0, icon: "script/image/usine.png" },
-    { name: "Temple à croquettes", price: 50000, mult: 0, auto: 300, owned: 0, icon: "script/image/temple.png" },
-    { name: "Dimension de croquettes", price: 50000, mult: 0, auto: 500, owned: 0, icon: "script/image/dimension.png" },
-    { name: "Univers de croquettes", price: 100000, mult: 0, auto: 1000, owned: 0, icon: "script/image/univers.png" },
-    { name: "Galaxy de croquettes", price: 500000, mult: 0, auto: 5000, owned: 0, icon: "script/image/galaxy.png" }
+    { name: "Cage à croquette", price: 50, mult: 5, auto: 0, owned: 0, icon: "script/image/cage.png" },
+    { name: "Paquet de croquette", price: 100, mult: 0, auto: 1, owned: 0, icon: "script/image/paquet.png" },
+    { name: "Magasin de croquettes", price: 500, mult: 0, auto: 5, owned: 0, icon: "script/image/magasin.png" },
+    { name: "Supermarcher de croquette", price: 1000, mult: 0, auto: 10, owned: 0, icon: "script/image/supermarche.png" },
+    { name: "Hypermarcher de croquettes", price: 2000, mult: 0, auto: 20, owned: 0, icon: "script/image/hypermarcher.png" },
+    { name: "Entrepot de croquettes", price: 10000, mult: 0, auto: 50, owned: 0, icon: "script/image/entrepot.png" },
+    { name: "Usine de croquettes", price: 100000, mult: 0, auto: 100, owned: 0, icon: "script/image/usine.png" },
+    { name: "Temple à croquettes", price: 500000, mult: 0, auto: 300, owned: 0, icon: "script/image/temple.png" },
+    { name: "Dimension de croquettes", price: 500000, mult: 0, auto: 500, owned: 0, icon: "script/image/dimension.png" },
+    { name: "Univers de croquettes", price: 1000000, mult: 0, auto: 1000, owned: 0, icon: "script/image/univers.png" },
+    { name: "Galaxy de croquettes", price: 5000000, mult: 0, auto: 5000, owned: 0, icon: "script/image/galaxy.png" }
   ];
 
   // basePrice to restore on reset/load
@@ -66,7 +66,7 @@
       window.BountyGame.count -= item.price;
       if (item.mult > 0) window.BountyGame.multiplier += item.mult;
       item.owned += 1;
-      item.price = Math.round(item.price * 1.15);
+      item.price = Math.round(item.price * 1.40);
 
       // purchase flash
       const node = storeDiv.children[idx];
